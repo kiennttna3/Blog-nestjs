4c615e2faae5cf822f0c92fba663fb629d04e84d
kiennttna3 <kiennttna3@gmail.com>
2025-04-22 19:50:06 +0700

    JWT Authentication, Login, Refresh Token API, Pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/entities/user.entity';
 
@@ -12,6 +13,22 @@ export class AuthController {
     @Post('register')
     register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
         console.log(registerUserDto)
+
         return this.authService.register(registerUserDto)
+    }
+
+    @Post('login')
+    @UsePipes(ValidationPipe)
+    login(@Body() LoginUserDto: LoginUserDto): Promise<any> {
+        console.log(LoginUserDto)
+        
+        return this.authService.login(LoginUserDto)
+    }
+
+    @Post('refresh-token')
+    refreshToken(@Body() { refresh_token }): Promise<any> {
+        console.log('refresh token')
+
+        return this.authService.refreshToken(refresh_token)
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../user/entities/user.entity';
 import { RegisterUserDto } from './dto/register-user.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User) private userRepository: Repository<User>,
+        private jwtService: JwtService,
+        private configService: ConfigService
     ){}
 
     async register(registerUserDto: RegisterUserDto): Promise<User> {
         const hashPassword = await this.hashPassword(registerUserDto.password);
+
         return await this.userRepository.save({
             firstName: registerUserDto.first_name,
             lastName: registerUserDto.last_name,
@@ -23,6 +30,80 @@ export class AuthService {
             created_at: new Date(),
             updated_at: new Date()
         });
+    }
+
+    async login(loginUserDto: LoginUserDto): Promise<any> {
+        const user = await this.userRepository.findOne(
+            { 
+                where: { 
+                    email: loginUserDto.email 
+                } 
+            }
+        )
+
+        if (!user) {
+            throw new HttpException("Email is not exist", HttpStatus.UNAUTHORIZED)
+        }
+
+        const checkPass = bcrypt.compareSync(loginUserDto.password, user.password);
+        if (!checkPass) {
+            throw new HttpException("Password is not correct", HttpStatus.UNAUTHORIZED)
+        }
+
+        // generate access token and refresh token
+        const payload = {
+            id: user.id,
+            email: user.email
+        }
+
+        return await this.generateToken(payload);
+    }
+
+    async refreshToken(refresh_token: string): Promise<any> {
+        try {
+            const verify = await this.jwtService.verifyAsync(refresh_token, {
+                secret: this.configService.get<string>('SECRET')
+            })
+            console.log(verify)
+
+            const checkExitToken = await this.userRepository.findOneBy({
+                email: verify.email,
+                refresh_token: refresh_token
+            })
+
+            if (checkExitToken) {
+                return this.generateToken({
+                    id: verify.id,
+                    email: verify.email
+                });
+            } else {
+                throw new HttpException("Refresh token is not valid", HttpStatus.UNAUTHORIZED)
+            }
+
+        } catch(error) {
+            console.log(error)
+            
+            throw new HttpException("Refresh token is not valid", HttpStatus.UNAUTHORIZED)
+        }
+    }
+
+    private async generateToken(payload: { id: number, email: string }) {
+        const access_token = await this.jwtService.signAsync(payload);
+        const refresh_token = await this.jwtService.signAsync(payload, {
+            secret: this.configService.get<string>('SECRET'),
+            expiresIn: this.configService.get<string>('EXP_IN_REFRESH_TOKEN')
+        });
+
+        await this.userRepository.update(
+            {
+                email: payload.email
+            },
+            {
+                refresh_token: refresh_token
+            }
+        )
+
+        return { access_token, refresh_token };
     }
 
     private async hashPassword(password: string): Promise<string> {
